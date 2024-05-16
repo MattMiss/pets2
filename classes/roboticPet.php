5ce6e2ab9a2aa7b0d4b24869ef65a7474c79c2b3
Mason Sain <sainmason012@gmail.com>
2024-05-16 14:25:44 -0400

    Changed object workings

--- classes/roboticPet.php
+++ classes/roboticPet.php
@@ -4,10 +4,30 @@ class RoboticPet extends Pet{
     private $_accessories;
 
     /**
-     * @param $_accessories
+     * @param string $_animal
+     * @param string $_color
+     * @param array $_accessories
      */
-    public function __construct($_accessories = "")
+    public function __construct($_animal = "", $_color = "", array $_accessories = array())
     {
+        parent::__construct($_animal, $_color);
         $this->_accessories = $_accessories;
     }
+
+    /**
+     * @return mixed|string
+     */
+    public function getAccessories()
+    {
+        return $this->_accessories;
+    }
+
+    /**
+     * @param mixed|string $accessories
+     */
+    public function setAccessories($accessories)
+    {
+        $this->_accessories = $accessories;
+    }
+
 }
--- classes/stuffedpet.php
+++ classes/stuffedpet.php
@@ -1,21 +1,70 @@
 <?php
 
-class StuffedPet {
+class StuffedPet extends Pet {
     private $_size;
     private $_stuffingType;
     private $_material;
 
     /**
-     * @param $_size
-     * @param $_stuffingType
-     * @param $_material
+     * @param string $_animal
+     * @param string $_color
+     * @param string $_size
+     * @param string $_stuffingType
+     * @param string $_material
      */
-    public function __construct($_size="", $_stuffingType="", $_material="")
+    public function __construct($_animal = "", $_color = "", $_size = "", $_stuffingType = "", $_material = "")
     {
+        parent::__construct($_animal, $_color);
         $this->_size = $_size;
         $this->_stuffingType = $_stuffingType;
         $this->_material = $_material;
     }
 
+    /**
+     * @return mixed|string
+     */
+    public function getSize()
+    {
+        return $this->_size;
+    }
 
+    /**
+     * @param mixed|string $size
+     */
+    public function setSize($size)
+    {
+        $this->_size = $size;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getStuffingType()
+    {
+        return $this->_stuffingType;
+    }
+
+    /**
+     * @param mixed|string $stuffingType
+     */
+    public function setStuffingType($stuffingType)
+    {
+        $this->_stuffingType = $stuffingType;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getMaterial()
+    {
+        return $this->_material;
+    }
+
+    /**
+     * @param mixed|string $material
+     */
+    public function setMaterial($material)
+    {
+        $this->_material = $material;
+    }
 }
