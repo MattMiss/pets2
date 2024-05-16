e81d5a4a8da600d466df1875768acd44b885cd5b
Matt Miss <matt.w.miss@gmail.com>
2024-05-16 13:48:34 -0400

    Add classes/ path to composer

--- classes/pet.php
+++ classes/pet.php
@@ -1,5 +1,6 @@
 <?php
 
+/** Pet class represents a pet */
 class Pet
 {
     private $_animal;
@@ -10,4 +11,38 @@ class Pet
         $this->_animal = $_animal;
         $this->_color = $_color;
     }
+
+    /**
+     * @return string The selected animal
+     */
+    public function getAnimal()
+    {
+        return $this->_animal;
+    }
+
+    /**
+     * @param string $animal The selected animal
+     */
+    public function setAnimal($animal)
+    {
+        $this->_animal = $animal;
+    }
+
+    /**
+     * @return string The selected color
+     */
+    public function getColor()
+    {
+        return $this->_color;
+    }
+
+    /**
+     * @param string $color The selected color
+     */
+    public function setColor($color)
+    {
+        $this->_color = $color;
+    }
+
+
 }
